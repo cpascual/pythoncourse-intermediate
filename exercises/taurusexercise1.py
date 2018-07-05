51a57665595b3dda62542ebd7332caf671a87193
Carlos Pascual <cpascual@cells.es>
2018-07-05 11:32:07 +0200

    Upload taurusexercise1 template

---
+++ exercises/taurusexercise1.py
@@ -0,0 +1,27 @@
+import sys
+from taurus.external.qt import QtGui
+from taurus.qt.qtgui.application import TaurusApplication
+
+
+class TaurusExercise1Widget(QtGui.QWidget):
+    """A specialized QLineEdit"""
+
+    def __init__(self, parent=None):
+
+        # use taurusexercise1.ui
+
+        # make that a button click sets the text of the lineEdit as
+        # the taurusLabel model
+
+
+
+if __name__ == "__main__":
+
+    # Initialize a Qt application (Qt will crash if you do not do this first)
+    app = TaurusApplication()
+    # instantiate the widget
+    w = TaurusExercise1Widget()
+    # show it (if you do not show the widget, it won't be visible)
+    w.show()
+    # Initialize the Qt event loop (and exit when we close the app)
+    sys.exit(app.exec_())
